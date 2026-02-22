2713c0f158a494dc756a0982441192904da41e08
Eben Lal <ebenlal58@gmail.com>
2026-02-22 23:26:46 +0530

    bug fix2

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -7,18 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
 
+# Initialize Django first
+django_application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_application,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter(chat.routing.websocket_urlpatterns)
     ),
 })
--- chat_project/settings.py
+++ chat_project/settings.py
@@ -145,9 +145,7 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
